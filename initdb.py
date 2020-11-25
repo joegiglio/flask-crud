45340966788bc775e07f121e24839ddfd72e8665
joegiglio <joe@joegiglio.org>
2020-11-24 19:59:53 -0500

    Updated initdb and README.

--- initdb.py
+++ initdb.py
@@ -1,13 +1,29 @@
+import os
 from app import db
+from flask import Flask
 from app import User
+from app import Dog
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from datetime import datetime
 
-db.drop_all()
-db.create_all()
+app = Flask(__name__)
+#db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-user = User(username="joe1", email="joe1@joe.com", level=1, created_at=datetime.utcnow())
+print("Running Flask-Migrate scripts...")
+
+os.system("flask db init")  # creates migrations directory
+os.system('flask db migrate -m "Initial migration."')  # creates migration files
+os.system("flask db upgrade")  # applies DB change
+
+print("Creating sample data...")
+
+user = User(username="joe1", email="joe1@joe.com", level=100, created_at=datetime.utcnow())
+dog = Dog(name="Romeo", breed="Maltese", age=12, created_at=datetime.utcnow())
 
 db.session.add(user)
+db.session.add(dog)
 db.session.commit()
 
-print("DB initialized.")
+print("Database initialized.")
